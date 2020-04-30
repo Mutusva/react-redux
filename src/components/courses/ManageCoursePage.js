b5f734161fa0883c5643f6113097d7d9be12adc1
Innocent <mutusvainno@gmail.com>
2020-04-30 18:14:37 +0200

    initial commit

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+// import { bindActionCreators } from "redux";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
+import Spinner from "../common/Spinner";
+import { toast } from "react-toastify";
+
+// get courses, authors, actions from props using object destructuring.
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  saveCourse,
+  history,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({ ...props.course });
+  const [saving, setSaving] = useState(false);
+
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch((error) => {
+        alert("Loading courses failed" + error);
+      });
+    } else {
+      setCourse({ ...props.course });
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => {
+        alert("Loading authors failed" + error);
+      });
+    }
+  }, [props.course]); // the empty array as a second aurgument to effect means the effect will run once when the component mounts
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse((prevCourse) => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value) : value,
+    }));
+  }
+
+  function formIsValid() {
+    debugger;
+    const { title, authorId, category } = course;
+    const errors = {};
+
+    if (!title) errors.title = "Title is required.";
+    if (!authorId) errors.author = "Author is required";
+    if (!category) errors.category = "Category is required";
+
+    setErrors(errors);
+    // Form is valid if the errors object still has no properties
+    return Object.keys(errors).length === 0;
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    if (!formIsValid()) return;
+
+    setSaving(true);
+    saveCourse(course)
+      .then(() => {
+        toast.success("Course Saved!");
+        history.push("/courses");
+      })
+      .catch((error) => {
+        setSaving(false);
+        setErrors({ onSave: error.message });
+      });
+  }
+
+  return authors.length === 0 || courses.length === 0 ? (
+    <Spinner />
+  ) : (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+      onSave={handleSave}
+      saving={saving}
+    />
+  );
+}
+
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
+};
+
+export function getCourseBySlug(courses, slug) {
+  return courses.find((course) => course.slug == slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courses.length > 0
+      ? getCourseBySlug(state.courses, slug)
+      : newCourse;
+
+  return {
+    course: course,
+    courses: state.courses,
+    authors: state.authors,
+  };
+}
+
+const mapDispatchToProps = {
+  // by declaring mapDispatchToProps as an object instead, each property
+  // will automatically be bound to dispatch.
+
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors,
+  saveCourse: courseActions.saveCourse,
+};
+
+const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
+export default connectedStateAndProps(ManageCoursePage);
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,13 @@
+// Root reduce
+import { combineReducers } from "redux";
+import courses from "./courseReducer";
+import authors from "./authorReducer";
+import apiCallsInProgress from "./apiStatusReducer";
+
+const rootReducer = combineReducers({
+  courses,
+  authors,
+  apiCallsInProgress,
+});
+
+export default rootReducer;
